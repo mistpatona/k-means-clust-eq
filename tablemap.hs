eab37283c9a0a8ac8cb06e512734f6bab49e75ab
Viktar Basharymau <6alliapumob@gmail.com>
2015-09-14 17:29:11 +0300

    Simplify getByKeyWithDefault a bit

--- tablemap.hs
+++ tablemap.hs
@@ -1,6 +1,5 @@
 import qualified Data.Map as M
 import Data.Map (Map)
-import Data.Maybe (fromMaybe)
 
 
 -- show multiple maps of Strings as Strings
@@ -11,4 +10,4 @@ uniteMaps d ms = map (getByKeyWithDefault d ms) ks
 
 getByKeyWithDefault :: Ord k => a -> [Map k a] -> k -> [a]
 getByKeyWithDefault def ms x = map f ms
-    where f = fromMaybe def . M.lookup x
+    where f = M.findWithDefault def x
